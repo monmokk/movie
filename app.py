b49e4527044037b75a4885cbc437005cbf8b93a8
mjk <myeongjikang910814@gmail.com>
2022-05-03 03:19:40 +0900

    co

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('')
+# 꼭 디비 주소 없애기ㅠㅠㅠ
 db = client.dbsparta
 
 import requests
